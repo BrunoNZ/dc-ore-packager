3c2846752b94b19f7356e9cff095978c27e3259a
Bruno Nocera Zanette <brunonzanette@gmail.com>
2019-10-08 18:17:52 -0300

    update IdentifierExceptions

--- identifier_exceptions.py
+++ identifier_exceptions.py
@@ -1,7 +1,7 @@
 exceptions = {
     "http://repositorio.unicamp.br":"localhost",
     "https://repositorio.ufrn.br":"repositorio.ufrn.br",
-    "https://tede.ufam.edu.br":"localhost",
+    "https://tede.ufam.edu.br":"tede.ufam.edu.br",
     "https://app.uff.br":"app.uff.br",
     "http://ri.ufmt.br":"localhost",
 }
