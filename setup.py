37f2c8c09166f76a0011f6b425865a7020682f36
Bruno Nocera Zanette <brunonzanette@gmail.com>
2019-11-25 22:53:52 -0300

    change "dc-ore-packager-BrunoNZ" to "dc-ore-packager" in Setup

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="dc-ore-packager-BrunoNZ",
+    name="dc-ore-packager",
     version="0.0.1",
     author="Bruno Nocera Zanette",
     author_email="brunonzanette@gmail.com",
