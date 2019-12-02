a71cd21b9b7170cbaab4a487087851e0e29f1e6c
Bruno Nocera Zanette <brunonzanette@gmail.com>
2019-12-01 22:38:35 -0300

    update VERSION to 0.0.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="dc-ore-packager",
-    version="0.0.1",
+    version="0.0.2",
     author="Bruno Nocera Zanette",
     author_email="brunonzanette@gmail.com",
     description="A simple DublinCore/ORE SimpleArchive Packager",
