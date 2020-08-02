168c7c7921ed83bc2c5ebd18ec70b3444474500d
--global <--global>
2020-08-01 23:47:01 -0300

    add useIdPrefix option
    Some repositories use a different handle prefix in OAI.
    This fixes it.

--- dc_ore_packager/DCOREPackager.py
+++ dc_ore_packager/DCOREPackager.py
@@ -21,12 +21,25 @@ class DCOREPackager:
 
     def __init__(
         self, baseURL, handleList,
-        verifySSL=True, idExceptions={}, outDir=None, outFile=None):
+        verifySSL=True,
+        idExceptions={},
+        outDir=None,
+        outFile=None,
+        useIdPrefix=False,
+        deleteOutfile=False,
+        debug=False):
 
+
+        # Debug options: Allow not delete file at end of execution
+        self.deleteOutfile = deleteOutfile
+        self.debug = debug
+
+        # Base URL
         self.baseURL = baseURL.rstrip('/')
 
         # OAI identifier exceptions
         self.idExceptions = idExceptions
+        self.useIdPrefix = useIdPrefix
 
         # Verify SSL flag:
         self.verifySSL = verifySSL
@@ -37,8 +50,14 @@ class DCOREPackager:
         # Verify SSL flag:
         self.verifySSL = verifySSL
 
+        # Register Namespaces in ElementTree
+        for prefix, uri in self.NAMESPACES.items():
+            ElementTree.register_namespace(prefix, uri)
+
+        # Get OAI Repository Identifier
         self.repositoryIdentifier = self.getOAIidentifier()
-        self.oaiIdentifierString = 'oai' + ':' + self.repositoryIdentifier + ':'
+        delim = self.repositoryIdentifier['delimiter']
+        self.oaiIdentifierString = 'oai' + delim + self.repositoryIdentifier['id'] + delim
 
         # Item Number. Used to create directories into ZipFile
         self.nItems = len(handleList)
@@ -47,13 +66,9 @@ class DCOREPackager:
         self.handle = []
         self.identifier = []
         for h in handleList:
-            handle = h.lstrip('/').rstrip('/')
+            handle = self.prepareHandle(h)
             self.handle.append(handle)
             self.identifier.append(self.oaiIdentifierString + handle)
-
-        # Register Namespaces in ElementTree
-        for prefix, uri in self.NAMESPACES.items():
-            ElementTree.register_namespace(prefix, uri)
 
         # Define outDir for getTempFile method
         self.outDir = outDir
@@ -65,8 +80,8 @@ class DCOREPackager:
         if (self.outFile is None):
             self.outFile = self.getTempFile()
 
-        # Debug option: Allow not delete file at end of execution
-        self.deleteOutfile = False
+        if (self.debug):
+            self.printDebug()
 
 
     def __del__(self):
@@ -78,8 +93,9 @@ class DCOREPackager:
         return Path(self.outDir+'/'+str(uuid.uuid4())+'.zip')
 
 
-    def getOAIidentifierException(self):
-        return self.idExceptions.get(self.baseURL)
+    def getIdentifierException(self, exceptType):
+        e = self.idExceptions.get(self.baseURL)
+        return e.get(exceptType) if e is not None else None
 
 
     def getOAIRequest(self,options):
@@ -89,21 +105,45 @@ class DCOREPackager:
 
     def getOAIidentifier(self):
 
-        # Verify if there is an exception for self.baseURL
-        idException = self.getOAIidentifierException()
-        if idException is not None:
-            return idException
-
+        oai_id = {}
+        
         options = {
             'verb': 'Identify'
         }
-        r = self.getOAIRequest(options)
-        xml = ElementTree.fromstring(r.content)\
-                .find('oai:Identify', namespaces=self.NAMESPACES)\
-                .find('oai:description', namespaces=self.NAMESPACES)\
-                .find('oai_id:oai-identifier', namespaces=self.NAMESPACES)\
-                .find('oai_id:repositoryIdentifier', namespaces=self.NAMESPACES)
-        return xml.text
+
+        # Verify if there is an exception for self.baseURL
+        idException = self.getIdentifierException("id")
+        if idException is not None:
+            oai_id['id'] = idException
+            oai_id['delimiter'] = ':'
+            oai_id['sample'] = None
+            oai_id['handlePrefix'] = None
+
+        else:
+            r = self.getOAIRequest(options)
+            xml = ElementTree.fromstring(r.content)\
+                    .find('oai:Identify', namespaces=self.NAMESPACES)\
+                    .find('oai:description', namespaces=self.NAMESPACES)\
+                    .find('oai_id:oai-identifier', namespaces=self.NAMESPACES)
+
+            oai_id['id'] = xml.find('oai_id:repositoryIdentifier', namespaces=self.NAMESPACES).text
+            oai_id['delimiter'] = xml.find('oai_id:delimiter', namespaces=self.NAMESPACES).text
+            oai_id['sample'] = xml.find('oai_id:sampleIdentifier', namespaces=self.NAMESPACES).text
+            oai_id['handlePrefix'] = oai_id['sample'].split(oai_id['delimiter'])[-1].split('/')[0]
+
+        if (self.debug):
+            print(r.content)
+            print(oai_id)
+
+        return oai_id
+
+
+    def prepareHandle(self, handle):
+        handle = handle.lstrip('/').rstrip('/')
+        useIdPrefix = self.useIdPrefix or self.getIdentifierException("useIdPrefix")
+        if useIdPrefix & (self.repositoryIdentifier['handlePrefix'] is not None):
+            handle = self.repositoryIdentifier['handlePrefix'] + '/' + handle.split('/')[-1]
+        return handle
 
 
     def writeContentsFile(self, outFile):
@@ -154,7 +194,7 @@ class DCOREPackager:
                 .find('oai:GetRecord', namespaces=self.NAMESPACES)\
                 .find('oai:record', namespaces=self.NAMESPACES)\
                 .find('oai:metadata', namespaces=self.NAMESPACES)
-                .find('dim:dim', namespaces=self.NAMESPACES)\
+                .find('dim:dim', namespaces=self.NAMESPACES)
             )
         dim_xml = self.convertDimToDc(xml)
         dim_xml.write(outFile, xml_declaration=True, encoding='utf-8')
@@ -173,7 +213,7 @@ class DCOREPackager:
                     .find('oai:GetRecord', namespaces=self.NAMESPACES)\
                     .find('oai:record', namespaces=self.NAMESPACES)\
                     .find('oai:metadata', namespaces=self.NAMESPACES)\
-                    .find('atom:entry', namespaces=self.NAMESPACES)\
+                    .find('atom:entry', namespaces=self.NAMESPACES)
                 )
         xml.write(outFile, encoding='utf-8')
         pass
@@ -199,3 +239,22 @@ class DCOREPackager:
 
         else:
             return self.outFile
+
+
+    def printDebug(self):
+        print("* self.baseURL", self.baseURL, sep=": ")
+        print("* self.idExceptions", self.idExceptions, sep=": ")
+        print("* self.useIdPrefix", self.useIdPrefix, sep=": ")
+        print("* self.verifySSL", self.verifySSL, sep=": ")
+        print("* self.oaiURL", self.oaiURL, sep=": ")
+        print("* self.headers", self.headers, sep=": ")
+        print("* self.verifySSL", self.verifySSL, sep=": ")
+        print("* self.repositoryIdentifier", self.repositoryIdentifier, sep=": ")
+        print("* self.oaiIdentifierString", self.oaiIdentifierString, sep=": ")
+        print("* self.nItems", self.nItems, sep=": ")
+        print("* self.handle", self.handle, sep=": ")
+        print("* self.identifier", self.identifier, sep=": ")
+        print("* self.outDir", self.outDir, sep=": ")
+        print("* self.outFile", self.outFile, sep=": ")
+        print("* self.deleteOutfile", self.deleteOutfile, sep=": ")
+        print("* self.debug", self.debug, sep=": ")
